9fa805a13dc98979cb53f5df9d5cdc64df3bb39f
Aryan <aryanramjasian@gmail.com>
2022-11-12 14:32:00 +0530

    Add function to JS file to play the games round
    A function has been added that deploys a randomiser for generating the computer choices of the game. The value the previous functon returns is used in a secondary function that takes user input and then deploys conditional statement to check who won the Rock-Paper-Scissor game round.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,25 @@
-console.log("working");
+function getComputerChoice(){
+    arr_Index=Math.floor((Math.random()*3));
+    arr_Options=["rock","paper","scissor"];
+    return arr_Options[arr_Index];
+}
+
+function playRound(userChoice,computerChoice){
+    userChoice=userChoice.toLowerCase();
+    if (userChoice===computerChoice){
+        console.log(`User chose : ${userChoice} \n`);
+        console.log(`Computer chose : ${computerChoice} \n`);
+        console.log("DRAW");
+    }
+    else if ((userChoice==="rock" && computerChoice==="scissor") || (userChoice==="paper" && computerChoice==="rock") || (userChoice==="scissor" && computerChoice==="paper")){
+        console.log(`User chose : ${userChoice} \n`);
+        console.log(`Computer chose : ${computerChoice} \n`);
+        console.log("User has won! \n");
+    }
+    else{
+        console.log(`User chose : ${userChoice} \n`);
+        console.log(`Computer chose : ${computerChoice} \n`);
+        console.log("Computer has won! \n");
+    }
+}
+playRound(prompt("Kindly chose between rock,paper and scissor"),getComputerChoice());
