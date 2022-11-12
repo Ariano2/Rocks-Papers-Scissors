67ef0ddc11af587983baea76171e4c8a04975658
Aryan <aryanramjasian@gmail.com>
2022-11-12 15:16:34 +0530

    Set up a scoreboard for the game
    The scoreboard runs using an array. The outcome of each round is stored to a variable, is passed to the array and then a prompt confirms if user wants to check the score or not. If they respond in the affirmative, the array is iterated element wise, value of each element is checked and a counter containing the number of appearances of each type of element is finally used to display the score in the console.

--- javascript.js
+++ javascript.js
@@ -10,21 +10,50 @@ function playRound(userChoice,computerChoice){
         console.log(`User chose : ${userChoice} \n`);
         console.log(`Computer chose : ${computerChoice} \n`);
         console.log("DRAW");
+        return "Draw";
     }
     else if ((userChoice==="rock" && computerChoice==="scissor") || (userChoice==="paper" && computerChoice==="rock") || (userChoice==="scissor" && computerChoice==="paper")){
         console.log(`User chose : ${userChoice} \n`);
         console.log(`Computer chose : ${computerChoice} \n`);
         console.log("User has won! \n");
+        return "User";
     }
     else{
         console.log(`User chose : ${userChoice} \n`);
         console.log(`Computer chose : ${computerChoice} \n`);
         console.log("Computer has won! \n");
+        return "Computer";
     }
 }
+final_Score=[];
 let iter="y";
+let roundCount=0;
 while (iter==="y"){
-    playRound(prompt("Kindly chose between rock,paper and scissor"),getComputerChoice());
+    let roundScore=playRound(prompt("Kindly chose between rock,paper and scissor"),getComputerChoice());
     iter=prompt("enter y to keep playing");
     console.log(iter);
+    final_Score[roundCount]=roundScore;
+    roundCount=roundCount+1;
 }
+let userWinCount=0;
+let computerWinCount=0;
+let drawCount=0;
+for (i=0;i<=((final_Score.length)-1);i=i+1){
+    if (final_Score[i]==="User"){
+        userWinCount=userWinCount+=1;
+    }
+    else if (final_Score[i]==="Computer")
+    {
+        computerWinCount+=1;
+    }
+    else{
+        drawCount+=1;
+    }
+}
+ask=prompt("Enter s to check final score");
+if (ask==="s"){
+    console.log(`number of draws were : ${drawCount} \n`);
+    console.log(`number of user wins were : ${userWinCount} \n`);
+    console.log(`number of computer wins were : ${computerWinCount} \n`);
+}
+else;
